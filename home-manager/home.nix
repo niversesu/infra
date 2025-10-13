7838f663aa7166acb845ed4e07222ed351842585
niversesu <niversesu@gmail.com>
2025-10-13 03:16:23 +0300

    bye bye chrome-remote-desktop

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -191,6 +191,18 @@ in {
     };
   };
 
+  # XDG settings
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/http" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/about" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/unknown" = "app.zen_browser.zen.desktop";
+    };
+  };
+
   # Services
   services.kdeconnect.enable = true;
 }
