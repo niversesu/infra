f367e2ef4376124ad1777d0bafa5485df70de930
niversesu <niversesu@gmail.com>
2025-10-24 09:50:44 +0300

    really cool changes again..x2

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -14,7 +14,7 @@
       ...
     }:
     {
-      nixosConfigurations.niver = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
