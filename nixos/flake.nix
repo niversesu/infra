3cd04d913901ce37159a86a0e635f0919b611423
niversesu <niversesu@gmail.com>
2025-10-24 09:46:31 +0300

    really cool changes again..

---
+++ nixos/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Homelab running Minecraft";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nix-minecraft,
+      ...
+    }:
+    {
+      nixosConfigurations.niver = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./minecraft.nix
+          nix-minecraft.nixosModules.minecraft-servers
+          {
+            nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+          }
+        ];
+      };
+    };
+}
