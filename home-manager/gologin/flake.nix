57383a06a40f74b9e8952213db3d72d89f90b7a1
niversesu <niversesu@gmail.com>
2025-11-08 17:48:12 +0300

    added gologin logo

--- home-manager/gologin/flake.nix
+++ home-manager/gologin/flake.nix
@@ -14,6 +14,11 @@
       sha256 = "sha256-O2T+/6qZsa3GWeyA0fSsGaKb8DCPDLX40fChPyLk1ug=";
     };
     
+    gologinLogo = pkgs.fetchurl {
+      url = "https://play-lh.googleusercontent.com/l7QfbzL4WA86ed1Op87TOAPn4Kx30vMYnczwPYq6A5H1MGB7CIigSdv-uYo4xiA_SQ=w240-h480-rw";
+      sha256 = "05ks8a4syispkh2v2rbvszk7niai8kgx9z2p1w9pn8a7kb6mifjc";
+    };
+    
     gologinAppImage = pkgs.stdenv.mkDerivation {
       name = "gologin-appimage";
       src = gologinTar;
@@ -35,6 +40,9 @@
       
       extraInstallCommands = ''
         mkdir -p $out/share/applications
+        mkdir -p $out/share/icons/hicolor/256x256/apps
+        cp ${gologinLogo} $out/share/icons/hicolor/256x256/apps/gologin.png
+        
         cat > $out/share/applications/gologin.desktop <<EOF
 [Desktop Entry]
 Name=GoLogin
