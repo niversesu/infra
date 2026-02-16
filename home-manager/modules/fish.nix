ffc82003afdb69b08e0b23f81a54c91a3f82acb9
niversesu <niversesu@gmail.com>
2026-02-16 03:29:45 +0300

    uhh.... no plugins?

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -7,30 +7,5 @@
       snrs = "sudo nixos-rebuild switch";
       hs = "home-manager switch";
     };
-    plugins = [
-      {
-        name = "tide";
-        src = pkgs.fetchFromGitHub {
-          owner = "IlanCosman";
-          repo = "tide";
-          rev = "v6.1.1";
-          sha256 = "sha256-ZyEk/WoxdX5Fr2kXRERQS1U1QHH3oVSyBQvlwYnEYyc=";
-        };
-      }
-    ];
-    interactiveShellInit = ''
-      set -gx tide_style Rainbow
-      set -gx tide_prompt_colors "True color"
-      set -gx tide_show_time "24-hour format"
-      set -gx tide_rainbow_prompt_separators Vertical
-      set -gx tide_powerline_prompt_heads Round
-      set -gx tide_powerline_prompt_tails Round
-      set -gx tide_powerline_prompt_style "Two lines, character"
-      set -gx tide_prompt_connection Disconnected
-      set -gx tide_powerline_right_prompt_frame No
-      set -gx tide_prompt_spacing Sparse
-      set -gx tide_icons "Many icons"
-      set -gx tide_transient Yes
-    '';
   };
 }
