94784596463cf18833d2aea8636432bf411b9a95
niversesu <niversesu@gmail.com>
2026-03-03 04:41:51 +0300

    muh fixes

--- nixos/modules/keyd.nix
+++ nixos/modules/keyd.nix
@@ -1,34 +1,28 @@
-{pkgs, lib, ...}: let
-  # Helper function to set all keys to noop
-  allNoop = keys: lib.foldl (acc: key: acc // {${key} = "noop";}) {} keys;
-
-  # Key groups
-  letters = lib.stringToCharacters "abcdefghijklmnopqrstuvwxyz";
-  numbers = map toString (lib.range 0 9);
-  fKeys = map (n: "f${toString n}") (lib.range 1 12);
-  modifierKeys = ["esc" "tab" "capslock" "leftshift" "rightshift" "leftcontrol" "rightcontrol" "leftalt" "rightalt" "leftmeta" "rightmeta"];
-  navKeys = ["space" "enter" "backspace" "insert" "delete" "home" "end" "pageup" "pagedown"];
-  arrowKeys = ["up" "down" "left" "right"];
-  symbolKeys = ["`" "-" "=" "[" "]" "\\" ";" "'" "," "." "/" "ro" "kpjpcomma" "yen"];
-
-  mainLayerKeys = letters ++ numbers ++ fKeys ++ modifierKeys ++ navKeys ++ arrowKeys ++ symbolKeys;
-in {
+{pkgs, lib, ...}: {
   services.keyd = {
     enable = true;
-    keyboards = {
-      internal = {
-        ids = ["0001:0001:d651c513"];
-        settings = {
-          main = allNoop mainLayerKeys;
+    keyboards.internal = {
+      ids = ["0001:0001:d651c513"];
+      settings = {
+        main = {
+          a = "noop"; b = "noop"; c = "noop"; d = "noop"; e = "noop"; f = "noop"; g = "noop"; h = "noop"; i = "noop"; j = "noop"; k = "noop"; l = "noop"; m = "noop"; n = "noop"; o = "noop"; p = "noop"; q = "noop"; r = "noop"; s = "noop"; t = "noop"; u = "noop"; v = "noop"; w = "noop"; x = "noop"; y = "noop"; z = "noop";
+          "1" = "noop"; "2" = "noop"; "3" = "noop"; "4" = "noop"; "5" = "noop"; "6" = "noop"; "7" = "noop"; "8" = "noop"; "9" = "noop"; "0" = "noop";
+          f1 = "noop"; f2 = "noop"; f3 = "noop"; f4 = "noop"; f5 = "noop"; f6 = "noop"; f7 = "noop"; f8 = "noop"; f9 = "noop"; f10 = "noop"; f11 = "noop"; f12 = "noop";
+          esc = "noop"; tab = "noop"; capslock = "noop"; leftshift = "noop"; rightshift = "noop"; leftcontrol = "noop"; rightcontrol = "noop"; leftalt = "noop"; rightalt = "noop"; leftmeta = "noop"; rightmeta = "noop";
+          space = "noop"; enter = "noop"; backspace = "noop";
+          insert = "noop"; delete = "noop"; home = "noop"; end = "noop"; pageup = "noop"; pagedown = "noop";
+          up = "noop"; down = "noop"; left = "noop"; right = "noop";
+          "`" = "noop"; "-" = "noop"; "=" = "noop"; "[" = "noop"; "]" = "noop"; "\\" = "noop"; ";" = "noop"; "'" = "noop"; "," = "noop"; "." = "noop"; "/" = "noop";
+          ro = "noop"; kpjpcomma = "noop"; yen = "noop";
         };
       };
-      external = {
-        ids = ["1a2c:0b2a:c4da6b8e"];
-        settings = {
-          main = {
-            numlock = "f11";
-            pause = "f12";
-          };
+    };
+    keyboards.external = {
+      ids = ["1a2c:0b2a:c4da6b8e"];
+      settings = {
+        main = {
+          numlock = "f11";
+          pause = "f12";
         };
       };
     };
--- nixos/modules/systemd.nix
+++ nixos/modules/systemd.nix
@@ -1,14 +1,14 @@
 { config, pkgs, lib, ... }:
 
 {
-  systemd.services.ntfsfix-sda5 = {
+  systemd.services.ntfsfix-sda4 = {
     description = "Run ntfsfix on /dev/sda4 at boot";
     wantedBy = [ "multi-user.target" ];
     after = [ "local-fs.target" ];
 
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.ntfs3g}/bin/ntfsfix -d /dev/sda5";
+      ExecStart = "${pkgs.ntfs3g}/bin/ntfsfix -d /dev/sda4";
     };
   };
 }
