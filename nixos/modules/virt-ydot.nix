bca34013ef0d577f0d9ecd44bcc517c52bffec4b
niversesu <niversesu@gmail.com>
2026-04-10 13:25:50 +0300

    virt-ydot seperation

--- nixos/modules/virt-ydot.nix
+++ nixos/modules/virt-ydot.nix
@@ -1,19 +1,28 @@
 { ... }: {
   flake.nixosModules.virt-ydot = { config, pkgs, lib, ... }: {
-    programs = {
-      virt-manager.enable = true;
-      ydotool.enable = true;
+    options.mySystem.virt-ydot = {
+      waydroid.enable  = lib.mkEnableOption "waydroid";
+      podman.enable    = lib.mkEnableOption "podman";
+      libvirt.enable   = lib.mkEnableOption "libvirt and virt-manager";
+      ydotool.enable   = lib.mkEnableOption "ydotool";
     };
-    environment.variables.YDOTOOL_SOCKET = lib.mkForce "/run/user/1000/.ydotool_socket";
 
-    virtualisation = {
-      waydroid.enable = true;
-      podman.enable = true;
-      libvirtd = {
+    config = {
+      virtualisation.waydroid.enable = lib.mkIf config.mySystem.virt-ydot.waydroid.enable true;
+
+      virtualisation.podman.enable = lib.mkIf config.mySystem.virt-ydot.podman.enable true;
+
+      programs.virt-manager.enable = lib.mkIf config.mySystem.virt-ydot.libvirt.enable true;
+      virtualisation.libvirtd = lib.mkIf config.mySystem.virt-ydot.libvirt.enable {
         enable = true;
         qemu.vhostUserPackages = with pkgs; [virtiofsd];
       };
+
+      programs.ydotool.enable = lib.mkIf config.mySystem.virt-ydot.ydotool.enable true;
+      environment.variables.YDOTOOL_SOCKET = lib.mkIf config.mySystem.virt-ydot.ydotool.enable
+        (lib.mkForce "/run/user/1000/.ydotool_socket");
+
+      networking.nftables.enable = false;
     };
-    networking.nftables.enable = false;
   };
 }
