72f567b0dd4153c28140090cc547884dfbc6711a
niversesu <niversesu@gmail.com>
2026-05-07 04:52:48 +0300

    add nix-gc alias and fix cachix-push flake reference

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -24,7 +24,8 @@
           snrs = "sudo nixos-rebuild switch --flake ~/infra#${config.myHome.fish.flakeTarget}";
           docker = "podman";
           nano = "nvim";
-          cachix-push = "cachix push niversesu $(nix path-info .#nixosConfigurations.#${config.myHome.fish.flakeTarget}.config.system.build.toplevel)";
+          cachix-push = "cachix push niversesu $(nix path-info .#nixosConfigurations.${config.myHome.fish.flakeTarget}.config.system.build.toplevel)";
+          nix-gc = "sudo nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo nix store optimise && sudo nix store gc";
         };
         interactiveShellInit = ''
           fish_config theme choose ${config.myHome.fish.theme}
