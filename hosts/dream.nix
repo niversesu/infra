ff2dfc69211246c73ef522125a90d33d4af659f8
niversesu <niversesu@gmail.com>
2026-05-11 04:39:44 +0300

    declared twice

--- hosts/dream.nix
+++ hosts/dream.nix
@@ -9,11 +9,6 @@
       user = "amani";
       host = "dream";
     };
-
-    boot.loader = {
-      systemd-boot.enable = lib.mkForce false;
-      grub.enable = lib.mkForce true;
-    };
     mySystem.host-dream-hw.enable = true;
     mySystem.gnome.enable = false;
     mySystem.plasma.enable = false;
