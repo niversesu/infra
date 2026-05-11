2a829f805837667973aba66d16badcab6bac757d
niversesu <niversesu@gmail.com>
2026-05-11 03:15:33 +0300

    amani fleshed out

--- home-manager/users/amani.nix
+++ home-manager/users/amani.nix
@@ -3,7 +3,15 @@
     imports = [
       self.homeModules.shared
     ];
-
+    myHome.fish = {
+      enable = true;
+      flakeTarget = "dream";
+      theme = "catppuccin-mocha";
+    };
+    myHome.theming = {
+      enable = true;
+      cursorName = "Bibata-Modern-Ice";
+    };
     myHome.packages.gaming.enable = true;
     myHome.packages.creative.enable = true;
     myHome.packages.tech-tools.enable = true;
