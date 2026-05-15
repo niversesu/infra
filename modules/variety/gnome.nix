38014f7079e3741f35614a34fcd4134223f76d6a
niversesu <niversesu@gmail.com>
2026-05-15 08:41:46 +0300

    removed unnecessary extensions

--- modules/variety/gnome.nix
+++ modules/variety/gnome.nix
@@ -13,11 +13,6 @@
           services = {
             displayManager.gdm.enable = true;
             desktopManager.gnome.enable = true;
-            gnome.gnome-remote-desktop.enable = true;
-
-            xrdp.enable = true;
-            xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
-            xrdp.openFirewall = true;
           };
 
           environment.gnome.excludePackages = with pkgs; [epiphany];
@@ -25,9 +20,6 @@
             gnomeExtensions.appindicator
             gnomeExtensions.copyous
             gnomeExtensions.blur-my-shell
-            gnomeExtensions.kiwi-is-not-apple
-            gnomeExtensions.kiwi-menu
-            gnome-remote-desktop
           ];
           programs.kdeconnect = {
             enable = true;
